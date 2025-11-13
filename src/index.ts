9873ff90437f856bdf9171bac2566abe20607d2c
Naihe <naiheemail@yeah.net>
2025-11-13 13:52:33 +0800

    build: CD

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { readFile } from 'fs/promises'
+import { join } from 'path'
 
 const regex = /(rgb\(\s*\d+\s*,\s*\d+\s*,\s*\d+\s*\)|#[0-9a-fA-F]{3,8})/g
 
 const getColor = async () => {
-  console.log('url:', import.meta.url)
-  const content = await readFile('dist/css/color.css', 'utf-8')
+  const _path = join(import.meta.url, 'css', 'color.css')
+  console.log('url:', _path)
+  const content = await readFile(_path, 'utf-8')
   const colors = content.match(regex)
   const hexColors = colors?.filter(c => c.startsWith('#'))
   const rgbColors = colors?.filter(c => !c.startsWith('#'))
