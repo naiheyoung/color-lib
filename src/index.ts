48748c1c24a670c26504dcc48aa49521dec51684
Naihe <naiheemail@yeah.net>
2025-11-13 14:32:42 +0800

    build: CD

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ const getColor = async () => {
     'css',
     'color.css'
   )
-  console.log(_path)
   const content = await readFile(_path, 'utf-8')
   const colors = content.match(regex)
   const hexColors = colors?.filter(c => c.startsWith('#'))
